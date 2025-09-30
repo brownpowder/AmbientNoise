cb6a5ac8c8864c00ffac909b303ca73700fb2ee7
brownpowder <brownpowder@gmail.com>
2025-09-30 12:22:39 +0900

    Initial Commit

---
+++ AmbientNoise/AmbientNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientNoiseApp.swift
+//  AmbientNoise
+//
+//  Created by KanayTakum on 2025/09/30.
+//
+
+import SwiftUI
+
+@main
+struct AmbientNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmbientNoiseTests/AmbientNoiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientNoiseTests.swift
+//  AmbientNoiseTests
+//
+//  Created by KanayTakum on 2025/09/30.
+//
+
+import Testing
+@testable import AmbientNoise
+
+struct AmbientNoiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
